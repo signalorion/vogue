539363c3294d818bfce6888719c86fdd24752a4f
signalorion <masa4@daum.net>
2020-09-17 15:46:28 +0900

    끝났당~

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 // VOGUE 메인 페이지 JS - main.js //
 
 $(function(){ /// jQB /////////////////
+    /*햄버거버튼 클릭시 모바일 메뉴 보이기*/
+    $("#ham").click(function(){
+        $(".mognb").slideToggle(700);
+    });
+    
+    /*검색버튼클릭시 모바일 검색창 보이기*/
+    $("#magni").click(function(){
+        $(".mos").slideToggle(200);
+    });
+    
     
     
 });///// jQB /////////////////////////
